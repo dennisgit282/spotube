8025818fe65339d003359afe54d904e00255d274
dennisgit282 <76529387+dennisgit282@users.noreply.github.com>
2024-01-02 18:28:13 -0500

    Update 1675256678_updated_tracks.js
    ticket 1006

--- server/pb_migrations/1675256678_updated_tracks.js
+++ server/pb_migrations/1675256678_updated_tracks.js
@@ -9,6 +9,7 @@ migrate((db) => {
 }, (db) => {
   const dao = new Dao(db)
   const collection = dao.findCollectionByNameOrId("pevn93oxbnovw0s")
+  // is this correct? There is an issue open saying the same song is being played rather than a collection. I am seeing "pevn93oxbnovw0s" in all the code files.
 
   collection.createRule = null
   collection.updateRule = null
